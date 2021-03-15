0f4dc3eb0e4f0b9e2eafa7658ee1c12767002813
TimWD <timgerm280@gmail.com>
2021-03-15 18:04:37 +0500

    created register and login pages

--- ribbon/urls.py
+++ ribbon/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import RibbonList 
+from .views import RibbonList, RegisterPage, RibbonLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', RibbonList.as_view(), name='index'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('login/', RibbonLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- ribbon/views.py
+++ ribbon/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from .models import Ribbon
 
 
@@ -7,3 +16,32 @@ class RibbonList(ListView):
     model = Ribbon
     template_name = 'ribbon/index.html'
     context_object_name = 'ribbons'
+
+
+class RegisterPage(FormView):
+    template_name = 'ribbon/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('index')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
+class RibbonLoginView(LoginView):
+    template_name = 'ribbon/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+    
