4cf8226ba24a32a05d8a411d0e39e089cdf62694
TimWD <timgerm280@gmail.com>
2021-04-05 10:33:34 +0500

    added static_root

--- firework/settings.py
+++ firework/settings.py
@@ -138,6 +138,8 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
