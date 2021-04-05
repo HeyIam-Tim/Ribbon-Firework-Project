06daf06350b1400a146d1bb4ad999e613b34d9bf
TimWD <timgerm280@gmail.com>
2021-04-05 11:22:34 +0500

    debug to True

--- firework/settings.py
+++ firework/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
